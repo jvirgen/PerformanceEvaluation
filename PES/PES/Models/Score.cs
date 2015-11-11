e37f7989901cd72807dd87336736d584e9cf000b
EduardoCortes <jose.cortes@4thsource.com>
2015-11-11 16:58:10 -0600

    Add classes Comment, Description, Score

---
+++ PES/PES/Models/Score.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PES.Models
+{
+    public class Score
+    {
+        public int ScoreId { get; set; }
+
+        public int DescriptionId { get; set; }
+
+        /// <summary>
+        /// Id Performance Evaluation
+        /// </summary>
+        public int PEId { get; set; }
+
+        public int ScoreEmployee { get; set; }
+
+        public int ScoreEvaluator { get; set; }
+
+        /// <summary>
+        /// Comments if the Scores are diferentes
+        /// </summary>
+        public string Comments { get; set; }
+
+        public int Calculation { get; set; }      
+        
+    }
+}
