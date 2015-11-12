78ccf0b010b871a1e585fd1140d15abe686fc5a9
angel5644 <victor_a_75@hotmail.com>
2015-11-12 17:53:29 -0600

    Enhancement #1 - comment from dev branch

--- PES/PES/Models/Employee.cs
+++ PES/PES/Models/Employee.cs
@@ -5,6 +5,9 @@ using System.Web;
 
 namespace PES.Models
 {
+    /// <summary>
+    /// This class will store a Employee
+    /// </summary>
     public class Employee
     {
         /// <summary>
@@ -12,24 +15,54 @@ namespace PES.Models
         /// </summary>
         public int EmployeeId { get; set; }
 
+        /// <summary>
+        /// First Name
+        /// </summary>
         public string FirstName { get; set; }
 
+        /// <summary>
+        /// Last Name
+        /// </summary>
         public string LastName { get; set; }
 
+        /// <summary>
+        /// Email
+        /// </summary>
         public string Email { get; set; }
 
+        /// <summary>
+        /// Employee Client 
+        /// </summary>
         public string Customer { get; set; }
 
+        /// <summary>
+        /// Position
+        /// </summary>
         public string Position { get; set; }
 
+        /// <summary>
+        /// Employee Profile Id
+        /// </summary>
         public int ProfileId { get; set; }
 
+        /// <summary>
+        /// Manager Id
+        /// </summary>
         public int ManagerId { get; set;}
 
+        /// <summary>
+        /// Hire Date
+        /// </summary>
         public DateTime HireDate { get; set; }
 
+        /// <summary>
+        /// Employee Ranking
+        /// </summary>
         public int Ranking { get; set;}
 
+        /// <summary>
+        /// End Date 
+        /// </summary>
         public DateTime EndDate { get; set; }
 
     }
