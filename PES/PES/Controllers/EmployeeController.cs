95e27d396f3fa19e4aa6e85f1a88aa708d5fb0c2
EduardoCortes <jose.cortes@4thsource.com>
2015-11-25 17:51:19 -0600

    change in employee contoller

--- PES/PES/Controllers/EmployeeController.cs
+++ PES/PES/Controllers/EmployeeController.cs
@@ -5,14 +5,14 @@ using System.Web;
 using System.Web.Mvc;
 using Oracle.ManagedDataAccess.Client;
 using Oracle.ManagedDataAccess.Types;
-
+using PES.Models;
 
 namespace PES.Controllers
 {
     public class EmployeeController : Controller
     {
         // GET: Employee
-        public string Index()
+        public ActionResult Index()
         {
             string EmployeeName = "";
 
@@ -22,17 +22,41 @@ namespace PES.Controllers
             Connection.Open();
 
             // Read data
-            OracleCommand Comand = new OracleCommand("SELECT FIRST_NAME FROM PES.EMPLOYEE", Connection);
+            string query = "SELECT ID_EMPLOYEE, " +
+                                  "FIRST_NAME," +
+
+
+            OracleCommand Comand = new OracleCommand("SELECT * FROM PES.EMPLOYEE", Connection);
             OracleDataReader Read = Comand.ExecuteReader();
-            
+
+            List<Employee> employees = new List<Employee>();
+            Employee employee;
             // Store sata
             while (Read.Read())
             {
-                EmployeeName = Convert.ToString(Read["FIRST_NAME"]);       
+                // Store data in employee object 
+                employee = new Employee();
+                employee.EmployeeId = Convert.ToInt32(Read["ID_EMPLOYEE"]);
+                employee.FirstName = Convert.ToString(Read["FIRST_NAME"]);
+                employee.LastName = Convert.ToString(Read["LAST_NAME"]);
+                employee.Email = Convert.ToString(Read["EMAIL"]);
+                employee.Customer = Convert.ToString(Read["CUSTOMER"]);
+                employee.Position = Convert.ToString(Read["POSITION"]);
+                employee.ProfileId = Convert.ToInt32(Read["ID_PROFILE"]);
+                employee.ManagerId = Convert.ToInt32(Read["iD_MANAGER"]);
+                employee.HireDate = Convert.ToDateTime(Read["HIRE_DATE"]);
+                employee.Ranking = Convert.ToInt32(Read["RANKING"]);
+                employee.EndDate = null;
+                // ---
+                
+      
+                // Add employee to the list
+                employees.Add(employee);
             }
 
             Connection.Close();
-            return "Employee name: " + EmployeeName;
+
+            return View(employees);
         }
     }
 }
