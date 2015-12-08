69ddb6b118038688412d88f94d84b0e3b73130ed
EduardoCortes <jose.cortes@4thsource.com>
2015-12-08 14:59:05 -0600

    Added Session Varaibles

--- PES/PES/Controllers/LoginUserController.cs
+++ PES/PES/Controllers/LoginUserController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using PES.Models;
 
 namespace PES.Controllers
 {
@@ -16,19 +17,49 @@ namespace PES.Controllers
         }
 
         [HttpPost]
-        public ActionResult Login(Models.User user)
+        public ActionResult Login(User user)
         {
+            //Check if the Email and Password exist in Office 356
             if (user.Authentication(user.UserName, user.Password))
             {
-                if(Convert.ToInt32(user.UserProfile)==1)
-                return RedirectToAction("Index", "PerformanceEvaluation");
+                // Get profile
+                PES.Models.User.ProfileUser userProfile = (PES.Models.User.ProfileUser)int.Parse((SessionProfile(user)));
 
+                //Deside if the user is a Resouce
+                if (userProfile == PES.Models.User.ProfileUser.Resource)
+                {
+                    //Returns a resourse's view
+                    return RedirectToAction("ChoosePeriod", "PerformanceEvaluation");
+                }
+                //Check if the user is a Manager or Director 
+                else if (userProfile == PES.Models.User.ProfileUser.Manager || userProfile == PES.Models.User.ProfileUser.Manager)
+                {
+                    //Returns a Manager's view
+                    return RedirectToAction("Index", "PerformanceEvaluation");
+                }
             }
             else
             {
-                ModelState.AddModelError("", "User or Passwore is incorrect");
+                //Return the login view if the user isn't valid 
+                return RedirectToAction("Login", "LoginUser");
             }
-           return RedirectToAction("Login", "LoginUser");
+            return RedirectToAction("Login", "LoginUser");
+        }
+
+        //Get session variables Profile
+        public string SessionProfile (User user)
+        {
+            Session["UserProfile"] = user.UserProfile(user.UserName);
+            string SessionProfile = (string)Session["UserProfile"];
+            return SessionProfile;
+        }
+
+        //Get session variable Email
+        public string SessionEmail(User user)
+        {
+            Session["UserEmail"] = user.UserName;
+            string SessionEmail = (string)Session["UserEmail"];
+            return SessionEmail;
         }
     }
 }
--- PES/PES/Models/User.cs
+++ PES/PES/Models/User.cs
@@ -11,9 +11,12 @@ namespace PES.Models
 {
     public class User
     {
+        //Get UserName and Password 
         public string UserName { get; set; }
         public string Password { get; set; }
+        public string Profile;
 
+        //Authentication Office 365
         public bool Authentication(string username, string pass)
         {
             try
@@ -36,22 +39,32 @@ namespace PES.Models
 
         }
 
-        public String UserProfile(string username)
+        //Get ID_Profile from the DB 
+        public string UserProfile(string UserName)
         {
-            System.Web.HttpContext.Current.Session["Name"] = username;
             OracleConnection Connection = new OracleConnection();
             Connection.ConnectionString = "data source=localhost;user id=system;password=4colima";
             Connection.Open();
-            string Query = "SELECT ID_PROFILE FROM PES.EMPLOYEE WHERE EMAIL="+"'"+username+"'";
+            string Query = "SELECT ID_PROFILE FROM PES.EMPLOYEE WHERE EMAIL="+"'"+UserName+"'";
             OracleCommand Comand = new OracleCommand(Query, Connection);
             OracleDataReader Read = Comand.ExecuteReader();
+           
             while (Read.Read())
             {
-                System.Web.HttpContext.Current.Session["Profile"]= Read["ID_PROFILE"];
+                Profile = Convert.ToString(Read["ID_PROFILE"]); 
             }
-
+            
             Connection.Close();
-            return "hola";
+
+            return Profile;
        }
+        
+        public enum ProfileUser
+        {
+            None = 0,
+            Resource = 1,
+            Manager = 2,
+            Director = 3
+        }
     }
 }
