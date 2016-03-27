36872611ec557717380c23443808d4e8082c1988
Karen Fletes <karenstephs@gmail.com>
2016-03-27 16:40:31 -0600

    commit update employee

--- PES/PES/Controllers/EmployeeController.cs
+++ PES/PES/Controllers/EmployeeController.cs
@@ -122,6 +122,92 @@ namespace PES.Controllers
             return RedirectToAction("ViewEmployees");
         }
 
+        [HttpGet]
+        public ActionResult UpdateEmployee(int id)
+        {
+            UpdateEmployeeViewModel model = new UpdateEmployeeViewModel();
+
+            // Get current user  
+            Employee currentUser = new Employee();
+            EmployeeService EmployeeService = new EmployeeService();
+            currentUser = EmployeeService.GetByEmail((string)Session["UserEmail"]);
+
+            // Get profiles
+            // test empty list of profiles, replace this line with a call to service to get the profiles
+            var profiles = _profileService.GetAllProfiles();
+
+            // Populate profiles 
+            List<SelectListItem> profilesList = new List<SelectListItem>();
+            foreach (var profile in profiles)
+            {
+                var newItem = new SelectListItem()
+                {
+                    Text = profile.Name,
+                    Value = (profile.ProfileId).ToString(),
+                    Selected = false
+                };
+                profilesList.Add(newItem);
+            }
+
+            // Get managers 
+            List<Employee> managers = _employeeService.GetAll();
+
+            //Populate managers
+            List<SelectListItem> managersList = new List<SelectListItem>();
+            foreach (var manager in managers)
+            {
+                var newItem = new SelectListItem()
+                {
+                    Text = manager.FirstName + " " + manager.LastName,
+                    Value = (manager.EmployeeId).ToString(),
+                    Selected = false
+                };
+                managersList.Add(newItem);
+            }
+
+            #region Set data
+            // Set profiles
+            model.ListProfiles = profilesList;
+            model.ListManagers = managersList;
+            #endregion
+
+            if (currentUser.ProfileId == (int)ProfileUser.Resource)
+            {
+                // user is resource not allowed, return to home  
+                // send error
+                //TempData["Error"] = "You're resource. You're not allowed to insert employees.";
+                return RedirectToAction("Index", "PerformanceEvaluation");
+            }
+
+            ViewBag.currentUserProfileId = currentUser.ProfileId;
+            return View(model);
+        }
+
+        [HttpPost]
+        public ActionResult UpdateEmployee(UpdateEmployeeViewModel employeeModel)
+        {
+            Employee newEmployee = new Employee();
+
+            newEmployee.EmployeeId = employeeModel.EmployeeId;
+            newEmployee.FirstName = employeeModel.FirstName;
+            newEmployee.LastName = employeeModel.LastName;
+            newEmployee.Email = employeeModel.Email;
+            newEmployee.ProfileId = employeeModel.SelectedProfile;
+            newEmployee.ManagerId = employeeModel.SelectedManager;
+            newEmployee.HireDate = employeeModel.HireDate;
+            newEmployee.EndDate = employeeModel.EndDate;
+            newEmployee.Customer = employeeModel.Customer;
+            newEmployee.Position = employeeModel.Position;
+            newEmployee.Project = employeeModel.Project;
+
+            _employeeService.UpdateEmployee(newEmployee);
+
+            // Add success message
+            //TempData["notice"] = "Successfully registered";
+
+            return RedirectToAction("ViewEmployees");
+        }
+
         public ActionResult ViewEmployees()
         {
             EmployeeService ViewEmployees = new EmployeeService();
---
+++ PES/PES/ViewModels/UpdateEmployeeViewModel.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using PES.Models;
+using System.Web.Mvc;
+using System.ComponentModel.DataAnnotations;
+
+namespace PES.ViewModels
+{
+    public class UpdateEmployeeViewModel
+    {
+        public int EmployeeId { get; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Customer { get; set; }
+        public string Email { get; set; }
+        public List<SelectListItem> ListProfiles { get; set; }
+        public int SelectedProfile { get; set; }
+        [DataType(DataType.Date)]
+        public DateTime HireDate { get; set; }
+        [DataType(DataType.Date)]
+        public DateTime EndDate { get; set; }
+        public string Position { get; set; }
+        public string Project { get; set; }
+        public List<SelectListItem> ListManagers { get; set; }
+        public int SelectedManager { get; set; }
+    }
+}
