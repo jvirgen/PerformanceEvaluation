42613e4f03e47234720484f440ba91086abe4d3c
Jorge Eduado <duardo_mg@hotmail.com>
2016-04-04 12:48:59 -0500

    Commit to do pull

--- PES/PES/ViewModels/ChangeProfileViewModel.cs
+++ PES/PES/ViewModels/ChangeProfileViewModel.cs
@@ -2,11 +2,14 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Web.Mvc;
+using System.ComponentModel.DataAnnotations;
 
 namespace PES.ViewModels
 {
     public class ChangeProfileViewModel : InsertEmployeeViewModel
     {
+        [Display(Name = "Transfer all to ")]
         public int NewManager { get; set; }
     }
 }
