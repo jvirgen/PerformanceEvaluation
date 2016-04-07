219bd1db4da0e4dcdea486c0f7374863e243df23
Jorge Eduado <duardo_mg@hotmail.com>
2016-04-07 10:57:21 -0500

    Commit

--- PES/PES/Scripts/enable-disable.js
+++ PES/PES/Scripts/enable-disable.js
@@ -1,19 +1,11 @@
 ﻿$(document).ready(function () {
-    $("DisableOption").click(function () {
-        var email = ($(this).parent().siblings().first().next().next().text().trim());
+    $("#DisableOption").click(function () {
+            $(this).parent().hide();
+            $(this).parent().siblings(".DisableActions").show();
+    });
 
-        $.getJSON('/Employee/GetEmployeeStatus?email=' + email, function (data) {
-
-            if (data.employees.EndDate != null) {
-                $(this).text("Enable");
-                $(this).parent().siblings().text().css({ "text-decoration": "line-through" });
-            }
-            else if (data.employees.EndDate == null) {
-                $(this).text("Disable");
-                $(this).parent().siblings().text().css({ "text-decoration": "none" });
-               
-            }
-
-        });
+    $("#EnableOption").click(function () {
+            $(this).parent.hide();
+            $(this).parent().siblings(".EnableActions").show();
     });
 });
