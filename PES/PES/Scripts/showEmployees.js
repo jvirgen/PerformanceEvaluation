149517a199c897c2050cb537c65c1265f86fd269
Karen F <anyn_16@hotmail.com>
2016-04-14 13:06:00 -0500

    commit showEmployees dsable script

--- PES/PES/Scripts/showEmployees.js
+++ PES/PES/Scripts/showEmployees.js
@@ -3,21 +3,27 @@
     var disableCheck = $('#showDisables').checked;
 
     if (enableCheck) {
-        $('#table-employees').children('tbody').remove();
-        $.getJSON('/Employee/GetEnabledEmployees', function (data) {
-
         
+
+        //call ajax
+        $.ajax({
+            type: "POST",
+            url: "Employees/GetEnabledEmployees",
+            contentType: 'application/json; charset=utf-8',
+            success: function (data) {
+                if (data)
+                    $('#table-employees').children('tbody').remove();
+            }        
         });
     }
     else if(disableCheck){
-        $('#table-employees').children('tbody').remove();
-        $.getJSON('/Employee/GetEmployeesProifile?profile=', function (data) {
+        //$('#table-employees').children('tbody').remove();
+        //$.getJSON('/Employee/GetEmployeesProifile?profile=', function (data) {
 
             //ciclo
         
-        });
+       
     
     }
 
     });
-});
