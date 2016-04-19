05dfb1fcc8ccf2f6eec129d8deef85852f2ba155
Jorge Eduado <duardo_mg@hotmail.com>
2016-04-19 11:16:18 -0500

    Commit

--- PES/PES/Scripts/_references.js
+++ PES/PES/Scripts/_references.js
@@ -18,6 +18,5 @@
 /// <reference path="modernizr-2.6.2.js" />
 /// <reference path="respond.js" />
 /// <reference path="select2.js" />
-/// <reference path="showedemployees.js" />
 /// <reference path="showemployees.js" />
 /// <reference path="validations.js" />
