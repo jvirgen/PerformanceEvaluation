8e04aff8d65153fb8619fd0593a416f6da698e9d
Jorge Eduado <duardo_mg@hotmail.com>
2016-04-22 12:10:42 -0500

    Commit fixed triking text in filtered views

--- PES/PES/Scripts/enable-disable.js
+++ PES/PES/Scripts/enable-disable.js
@@ -1,8 +1,4 @@
-﻿$(document).ready(function () {
-});
-
-function enableDisable(employeeId, option) {
-
+﻿function enableDisable(employeeId, option) {
     // Call ajax
     $.ajax({
         url: '/Employee/EnableDisableEmployee',
@@ -13,6 +9,7 @@ function enableDisable(employeeId, option) {
         // Get data from json
         // Call was successfully executed
         if (data.success) {
+            var element = $('#Id'+ employeeId).siblings('td:last').children('.enableDisable');
             var rowElement = element.parent().siblings();
 
             if (option == "enable") {
@@ -25,7 +22,7 @@ function enableDisable(employeeId, option) {
                 element.attr("data-option", "disable");
 
                 // Enable edit button
-                var editElement = rowElement.parent().find(".edit-button").first();
+                var editElement = element.siblings('.edit-button');
                 editElement.attr("disabled", false);
             }
             else { // Disable
@@ -37,7 +34,7 @@ function enableDisable(employeeId, option) {
                 element.attr("data-option", "enable");
 
                 // Disable edit button
-                var editElement = rowElement.parent().find(".edit-button").first();
+                var editElement = element.siblings('edit-button');
                 editElement.attr("disabled", true);
             }
         } else {
@@ -48,54 +45,54 @@ function enableDisable(employeeId, option) {
     });
 }
 
-$(".enableDisable").on("click", function (e) {
-    // Link element clicked
-    var element = $(e.target);
+//$(".enableDisable").on("click", function (e) {
+//    // Link element clicked
+//    var element = $(e.target);
 
-    var option = element.attr("data-option");
-    var employeeId = element.attr("data-employee-id");
+//    var option = element.attr("data-option");
+//    var employeeId = element.attr("data-employee-id");
 
-    // Call ajax
-    $.ajax({
-        url: '/Employee/EnableDisableEmployee',
-        type: "POST",
-        data: { idEmployee: employeeId, option: option }
-    })
-    .done(function (data) {
-        // Get data from json
-        // Call was successfully executed
-        if (data.success) {
-            var rowElement = element.parent().siblings();
+//    // Call ajax
+//    $.ajax({
+//        url: '/Employee/EnableDisableEmployee',
+//        type: "POST",
+//        data: { idEmployee: employeeId, option: option }
+//    })
+//    .done(function (data) {
+//        // Get data from json
+//        // Call was successfully executed
+//        if (data.success) {
+//            var rowElement = element.parent().siblings();
 
-            if (option == "enable") {
+//            if (option == "enable") {
 
-                if (rowElement.hasClass("line-through")) {
-                    rowElement.removeClass("line-through");
-                }
+//                if (rowElement.hasClass("line-through")) {
+//                    rowElement.removeClass("line-through");
+//                }
 
-                element.text("Disable");
-                element.attr("data-option", "disable");
+//                element.text("Disable");
+//                element.attr("data-option", "disable");
 
-                // Enable edit button
-                var editElement = rowElement.parent().find(".edit-button").first();
-                editElement.attr("disabled", false);
-            }
-            else { // Disable
-                if (!rowElement.hasClass("line-through")) {
-                    rowElement.addClass("line-through");
-                }
+//                // Enable edit button
+//                var editElement = rowElement.parent().find(".edit-button").first();
+//                editElement.attr("disabled", false);
+//            }
+//            else { // Disable
+//                if (!rowElement.hasClass("line-through")) {
+//                    rowElement.addClass("line-through");
+//                }
 
-                element.text("Enable");
-                element.attr("data-option", "enable");
+//                element.text("Enable");
+//                element.attr("data-option", "enable");
 
-                // Disable edit button
-                var editElement = rowElement.parent().find(".edit-button").first();
-                editElement.attr("disabled", true);
-            }
-        } else {
-            alert("There was an error.");
-        }
-    })
-    .fail(function () {
-    });
-})
+//                // Disable edit button
+//                var editElement = rowElement.parent().find(".edit-button").first();
+//                editElement.attr("disabled", true);
+//            }
+//        } else {
+//            alert("There was an error.");
+//        }
+//    })
+//    .fail(function () {
+//    });
+//})
