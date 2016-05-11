c8765680a44f995fc7c1c2f5ba595477defe25b3
Karen F <anyn_16@hotmail.com>
2016-05-11 10:28:53 -0500

    commit period service created

---
+++ PES/PES/Services/PeriodService.cs
@@ -0,0 +1,58 @@
+﻿using Oracle.ManagedDataAccess.Client;
+using PES.DBContext;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PES.Services
+{
+    public class PeriodService
+    {
+        private PESDBContext dbContext;
+
+        public PeriodService()
+        {
+            dbContext = new PESDBContext();
+        }
+
+        //Get all periods
+        public List<Period> GetAll()
+        {
+            List<Period> periods = new List<Period>();
+            Period employee = new Period();
+            try
+            {
+                using (OracleConnection db = dbContext.GetDBConnection())
+                {
+                    db.Open();
+                    string Query = "SELECT ID_PERIOD, " +
+                                           "NAME " +
+                                           "FROM PERIOD";
+
+                    OracleCommand Comand = new OracleCommand(Query, db);
+                    OracleDataReader Read = Comand.ExecuteReader();
+                    while (Read.Read())
+                    {
+
+                        // Store data in period object 
+                        period = new Period();
+                        period.PeriodId = Convert.ToInt32(Read["ID_PERIOD"]);
+                        period.Name = Convert.ToString(Read["NAME"]);
+
+                        periods.Add(period);
+                    }
+                    db.Close();
+                }
+
+            }
+            catch (Exception ex)
+            {
+                throw;
+            }
+            return periods;
+        }
+    }
+
+   
+}
