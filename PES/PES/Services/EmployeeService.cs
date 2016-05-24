83274b184ae7fda918b3f07f5031a9f68a55a0fa
Jorge Eduado <duardo_mg@hotmail.com>
2016-05-24 12:53:24 -0500

    Commit fixed services to get location of an employee to show it in details view

--- PES/PES/Services/EmployeeService.cs
+++ PES/PES/Services/EmployeeService.cs
@@ -46,7 +46,8 @@ namespace PES.Services
                                            "ID_PROFILE," +
                                            "ID_MANAGER," +
                                            "END_DATE, " +
-                                           "PROJECT " +
+                                           "PROJECT, " +
+                                           "ID_LOCATION " +
                                            "FROM EMPLOYEE WHERE EMAIL = '" + email + "'";
 
                     OracleCommand Comand = new OracleCommand(Query, db);
@@ -66,6 +67,7 @@ namespace PES.Services
                         employee.ManagerId = Convert.ToInt32(Read["ID_MANAGER"]);
                         string endDate = Convert.ToString(Read["END_DATE"]);
                         employee.Project = Convert.ToString(Read["PROJECT"]);
+                        employee.LocationId = Convert.ToInt32(Read["ID_LOCATION"]);
 
                         if (!string.IsNullOrEmpty(endDate))
                         {
