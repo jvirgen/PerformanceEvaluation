d9f47460d11b8214d51b2d9286a8ba6e310acd56
Jorge Eduado <duardo_mg@hotmail.com>
2016-05-25 12:19:26 -0500

    Commit created script to selecte automatically current location

--- PES/PES/Scripts/_references.js
+++ PES/PES/Scripts/_references.js
@@ -15,6 +15,7 @@
 /// <reference path="jquery-1.10.2.js" />
 /// <reference path="jquery-1.11.3.min.js" />
 /// <reference path="jquery-ui-1.11.4.js" />
+/// <reference path="location.js" />
 /// <reference path="modernizr-2.6.2.js" />
 /// <reference path="moment.min.js" />
 /// <reference path="moment-with-locales.min.js" />
---
+++ PES/PES/Scripts/location.js
@@ -0,0 +1,67 @@
+﻿var currentLocation;
+if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(GeoSuccess, geoFail);
+}
+else {
+    alert("Sorry, your browser does not support geolocation services.");
+}
+
+function GeoSuccess(position) {
+    var lat = position.coords.latitude;
+    var lng = position.coords.longitude;
+    //alert("lat: " + lat + " lng: " + lng);
+    codeLatLng(lat, lng);
+}
+
+function geoFail() {
+    alert("Error while getting position");
+}
+
+
+var geocoder;
+function initialize() {
+    geocoder = new google.maps.Geocoder();
+}
+
+function codeLatLng(lat, lng) {
+
+    var latlng = new google.maps.LatLng(lat, lng);
+    geocoder.geocode({ 'latLng': latlng }, function (results, status) {
+        if (status == google.maps.GeocoderStatus.OK) {
+            console.log(results)
+            if (results[1]) {
+                //formatted address
+                //var address = results[0].formatted_address;
+                //alert("address = " + address);
+                currentLocation = results[5].formatted_address;
+                setUpDropDown(currentLocation);
+            } else {
+                alert("No results found");
+            }
+        } else {
+            alert("Geocoder failed due to: " + status);
+        }
+    });
+}
+
+function setUpDropDown(location){
+    var loc = location.split(",");
+    var city = loc[0];
+    var country = loc[1];
+
+    if (city == "Colima") {
+        $("#dropdownLocation option[value='1']").attr("selected", true);
+    }
+    else if (city == "Ciudad de México") {
+        $("#dropdownLocation option[value='2']").attr("selected", true);
+    }
+    else if (city == "Mérida") {
+        $("#dropdownLocation option[value='3']").attr("selected", true);
+    }
+    else if(country == "Estados Unidos"){
+        $("#dropdownLocation option[value='4']").attr("selected", true);
+    }
+    else {
+
+    }
+}
