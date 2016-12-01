a9191db938838e48bddf24f14f29d0c966b8ac6d
Rair15 <alan.guzman@4thsource.com>
2016-12-01 16:35:21 -0600

    removed resource name discard button to exit request view added partial view for dates section added partial view for cancelation alert modal fixed dates add remove buttons

--- PES/PES/Scripts/vacationsRequest.js
+++ PES/PES/Scripts/vacationsRequest.js
@@ -1,19 +1,64 @@
 ﻿$(document).ready(function () {
-
+    $(function() {
+        $('input[name="daterange"]').daterangepicker();
+    });
 });
 
 function addDate(btnAdd) {
+    $('#datesGroup').after('<hr class="divisor">' +
+                            '<div id="datesGroup" class="form-group">' +
+                                '<div id="subDatesGroup" class="form-group">' +
+                                    '<div id="datesCont" class="container flexEnd">' +
+                                        '<div class="col-md-3 text-center">' +
+                                            '<label for="start">Start Date - End Date</label>' +
+                                            '<input id="start" type="text" name="daterange" class="form-control text-center" data-val="true" data-val-required="Start date is required"/>' +
+                                            '<span class="field-validation-valid text-danger" data-valmsg-for="start" data-valmsg-replace="true"></span>' +
+                                        '</div>' +
+                                        '<div class="col-md-3">' +
+                                            '<label>Return Date</label>' +
+                                            '<input type="text" class="form-control" id="return" disabled="disabled"/>' +
+                                        '</div>' +
+                                        '<div class="col-md-1">' +
+                                            '<button onclick="addDate(this)" type="button" class="btn btn-default addBtn">' +
+                                                'Add Date' +
+                                            '</button>' +
+                                        '</div>' +
+                                    '</div>' +
+                                '</div>' +
+                                '<div id="subDatesGroup" class="form-group">' +
+                                    '<div id="datesCont" class="container flexCenter">' +
+                                        '<div id="leadName" class="col-md-4">' +
+                                            '<label for="lead" class="control-label">Lead Name</label>' +
+                                            '<div>' +
+                                                '<input id="lead" type="text" class="form-control" data-val="true" data-val-required="Lead name is required" />' +
+                                            '</div>' +
+                                            '<span class="field-validation-valid text-danger" data-valmsg-for="lead" data-valmsg-replace="true"></span>' +
+                                        '</div>' +
+                                        '<div id="checkarea" class="col-md-2 checkbox">' +
+                                            '<label id="checktext">' +
+                                                '<input type="checkbox" id="unpaid"/>I do not have a project' +
+                                            '</label>' +
+                                        '</div>' +
+                                        '<div class="removeBtn col-md-1">' +
+                                            '<button onclick="removeDate(this)" type="button" class="btn btn-danger">Remove Date</button>' +
+                                        '</div>' +
+                                    '</div>' +
+                                '</div>' +
+                            '</div>');
+    /*
     $('#datesGroup').after('<div id="datesGroup" class="form-group"><div id="datesCont" class="container flexEnd"><div class="col-md-3 text-center"><label for="start">Start Date - End Date</label><input id="start" type="text" name="daterange" class="form-control text-center" data-val="true" data-val-required="Start date is required"><span class="field-validation-valid text-danger" data-valmsg-for="start" data-valmsg-replace="true"></span></div><div class="col-md-2"><label>Return Date</label><input type="text" class="form-control" id="return"></div><div class="col-md-1"><button onclick="addDate(this)" type="button" class="btn btn-default addBtn">Add Date</button></div><div id="removeBtn" class="col-md-1"><button onclick="removeDate(this)" type="button" class="btn btn-danger">Remove Date</button></div></div></div>');
-
+    */
     $(function () {
         $('input[name="daterange"]').daterangepicker();
     });
 
     disableBtn();
+    showBtn();
 }
 
 function removeDate(btnRemove) {
-    $(btnRemove).parent().parent().parent().remove();//removeBtn->datesCont->datesGroup->remove()
+    $(btnRemove).parent().parent().parent().parent().prev('hr.divisor').remove();
+    $(btnRemove).parent().parent().parent().parent().remove();//removeBtn->datesCont->subdatesGroup->datesGroup->remove()
     enableBtn();
 }
 
@@ -27,11 +72,10 @@ function disableBtn() {
     }
 }
 
-function validateFields() {
-    var validator = $("#form").validate();
-    validator.element("#title");
-    validator.element("#lead");
-    validator.element("#date");
-    validator.element("#returnDate");
-    validator.element("#comments");
+function showBtn() {
+    for (var i = 0, l = $('.removeBtn').length - 1; i < l; i++) {
+            $('.removeBtn').eq(i).removeClass("hidden");
+    }
+
+    $('.removeBtn').eq($('.removeBtn').length - 1).addClass("hidden");
 }
