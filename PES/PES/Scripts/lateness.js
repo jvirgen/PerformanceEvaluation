9334b2882785ac841dbd3aa49027ba6b123aa4ef
vaes <vaes-123@hotmail.com>
2016-12-02 13:55:15 -0600

    #38 - charts for the current year and last 5 years added

--- PES/PES/Scripts/lateness.js
+++ PES/PES/Scripts/lateness.js
@@ -1,5 +1,12 @@
 ﻿$(function () {
 
+    $('#Tableview').DataTable({
+        dom: '<"row"<"col-md-6"B><"col-md-6"f>>',
+        buttons: [
+            'excel', 'pdf'],
+        pageLength: -1
+    });
+
     $('input[name="period"]').click(function () {
         var period = $('input[name="period"]:checked').val();
         $.ajax({
@@ -14,17 +21,114 @@
                     dom: '<"row"<"col-md-6"B><"col-md-6"f>>',
                     buttons: [
                         'excel', 'pdf'],
-                    pageLength: -1 
+                    pageLength: -1
                 });
-            
+
+                
+                if (period == "year") {
+                    chart("2016");
+                }
+                else if (period == "last 5 years") {
+                    paintSelect();
+                    chart("2016");
+                    $('select').on('change', function () {
+                        chart(this.value);
+                    });
+                }
+                
             }
         });
     });
 
-    $('#Tableview').DataTable({
-        dom: '<"row"<"col-md-6"B><"col-md-6"f>>',
-        buttons: [
-            'excel', 'pdf'],
-        pageLength: -1
-    });
+    function chart(year) {
+        
+        var month = {"January":0, "February":0, "March":0, "April":0, "May":0, "June":0, "July":0, "August":0, "September":0, "October":0, "November":0, "December":0};
+
+        //select the column date and compare the months
+        $('#Tableview tbody tr td:nth-child(1)').each(function () {            
+            var date = $.trim($(this).text()).toLowerCase();
+            for (var key in month) {
+                if (date.indexOf(key.toLowerCase()) >= 0 && date.indexOf(year) >= 0) {
+                    month[key] += 1;
+                }
+            }  
+        });
+      
+        Highcharts.chart('container', {
+            chart: {
+                type: 'column'
+            },
+            title: {
+                text: 'Leness Chart'
+            },
+            xAxis: {
+                type: 'category',
+                labels: {
+                    rotation: -45,
+                    style: {
+                        fontSize: '13px',
+                        fontFamily: 'Verdana, sans-serif'
+                    }
+                }
+            },
+            yAxis: {
+                min: 0,
+                title: {
+                    text: 'No. Lateness'
+                }
+            },
+            legend: {
+                enabled: false
+            },
+            tooltip: {
+                pointFormat: 'Lateness, <b>{point.y}</b>'
+            },
+            series: [{
+                name: 'Lateness',
+                data: [
+                    ['January', month["January"]],
+                    ['February', month["February"]],
+                    ['March', month["March"]],
+                    ['April', month["April"]],
+                    ['May', month["May"]],
+                    ['June', month["June"]],
+                    ['July', month["July"]],
+                    ['August', month["August"]],
+                    ['September', month["September"]],
+                    ['October', month["October"]],
+                    ['November', month["November"]],
+                    ['December', month["December"]]
+                ],
+                dataLabels: {
+                    enabled: true,
+                    rotation: -90,
+                    color: '#FFFFFF',
+                    align: 'right',
+                    format: '{point.y}',
+                    y: 10, 
+                    style: {
+                        fontSize: '13px',
+                        fontFamily: 'Verdana, sans-serif'
+                    }
+                }
+            }]
+        });
+    }
+
+    function paintSelect() {
+        var date = new Date();
+        var element = "<div class='form-group'>"+
+                        "<select class='form-control' id='sYear' style='margin-top:3%; margin-left:5%; width:100px;'>"+
+                            "<option>" + date.getFullYear() + "</option>"+
+                            "<option>" + (date.getFullYear()-1) + "</option>" +
+                            "<option>" + (date.getFullYear()-2) + "</option>" +
+                            "<option>" + (date.getFullYear()-3) + "</option>" +
+                            "<option>" + (date.getFullYear()-4) + "</option>" +
+                            "<option>" + (date.getFullYear()-5) + "</option>" +
+                        "</select>"+
+                       "</div>";
+        $("#container").before(element);
+    }
+    
+
 });
