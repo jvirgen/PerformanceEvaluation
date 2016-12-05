7b99766b9331b17f2af91e26b24def6bf2745d4f
IvanDenneb1 <ivandenneb@gmail.com>
2016-12-05 16:44:17 -0600

    lateness menu manager SPRINT2

--- PES/PES/Controllers/MenuController.cs
+++ PES/PES/Controllers/MenuController.cs
@@ -14,6 +14,29 @@ namespace PES.Controllers
             return View();
         }
 
-         
+        public ActionResult latenessMenu()
+        {
+            return View();
+        }
+
+
+
+
+        public ActionResult Choose()
+        {
+            //Get the session to make validations
+            int useProfile = (int)Session["UserProfile"];
+            string employeeEmail = (string)Session["UserEmail"];
+            int employeeID = (int)Session["UserId"];
+
+
+            //Redirecting to PerformanceEvaluation according USERPROFILE (1.-Resourse, 2.-Manager, 3.-Director)
+            if (useProfile == 1)
+            {
+                return RedirectToAction("Index", "Lateness", new { employeeEmail, employeeID });
+            }
+            else
+                return RedirectToAction("latenessMenu", "Menu", new { employeeEmail = Session["UserEmail"], employeeID = Session["UserId"] });
+        }
     }
 }
