833ac6cd061dead4d95c334b8a305743198d3f4a
jGutierrez <jesusgs5666@gmail.com>
2017-12-05 15:53:49 -0600

    Holidays view created (Index)

---
+++ PES/PES/Controllers/HolidaysController.cs
@@ -0,0 +1,30 @@
+﻿using PES.Models;
+using PES.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PES.Controllers
+{
+    public class HolidaysController : Controller
+    {
+        private HolidayService _holidayService;
+
+        public HolidaysController()
+        {
+            _holidayService = new HolidayService();
+        }
+
+        // GET: Holidays
+        public ActionResult Index()
+        {
+            IEnumerable<Holiday> holidays = new List<Holiday>();
+
+            holidays = _holidayService.GetAllHolidays();
+
+            return View(holidays);
+        }
+    }
+}
