32b854a14f10e5b64e4dce73be4bc801de83f654
alexmuoso <alex_mu_oso@hotmail.com>
2017-12-12 13:47:36 -0600

    Global Values, unique admin Email #63

---
+++ PES/PES/Models/Globals.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PES.Models
+{
+    public static class Globals
+    {
+        public static readonly string SMTPOutlookEmail = "alejandro.munoz@4thsource.com";
+        public static readonly string SMTPOutlookPass = "Alexmuoso123";
+    }
+}
