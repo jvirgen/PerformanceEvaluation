47336ca25ed3fbbb51ce6f32c82a206201b48eae
VictorMunguia <victor00171@hotmail.com>
2018-04-30 11:37:59 -0500

    Add modals and colors in the history

--- PES/PES/Scripts/vacationsRequest.js
+++ PES/PES/Scripts/vacationsRequest.js
@@ -182,7 +182,7 @@ function SendInfo(start, end) {
                     $("#daysReq").val(data.NumberDaysRequested);
                     break;
                 case 0:
-                    $("#correctDate").modal();
+
                     $("#daysReq").val(data.NumberDaysRequested);
                     $("#returnDay").val(data.ReturnDate)
                     $("#daysRequest").val(data.NumberDaysRequested)
