70e59dcdbc7627c3df6d4f1e445f1ac304c51ab6
ricardo4th <ricardo.regalado@4thsource.com>
2018-05-10 10:24:44 -0500

    Eliminated validation: i can't cancel the vacation request after two days. #137

--- PES/PES/Scripts/VacationRequestEmail.js
+++ PES/PES/Scripts/VacationRequestEmail.js
@@ -36,19 +36,6 @@ function sendedImail(id, VacDaysReq, stringFechas) {
 }
 
 
-$('#cancelBotomModal').on("click", function (e) {
-    var Fecha = $('#start').val();
-    var starDate = moment(Fecha.substring(0, 10));
-    var today = moment(new Date());
-    var compareToday = today.add(2,'days');
-    var uncomingDate = true;
-    //True es no puede cancelar si es false si puede cancelar
-    if (starDate < compareToday) {
-        uncomingDate = true;
-        $("#informationSoon").modal();
-    }
-    else {
-        uncomingDate = false;
-        $("#cancelFormModal").submit();
-    }
+$('#cancelBotomModal').on("click", function (e) {    
+        $("#cancelFormModal").submit();    
 });
